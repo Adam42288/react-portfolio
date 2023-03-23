a1cda9559694cd275c373ddb1afd6cb98603d82c
Adam <asantillana@apple.com>
2023-03-22 21:19:05 -0500

    Updated src

--- src/projects.js
+++ src/projects.js
@@ -6,7 +6,7 @@ const projects = [
       "This is a project we did for a class. It's a demo of how to make a website accessible.",
     image: require("./assets/accessibilityproject.png"),
     siteAddress: "https://adam42288.github.io/week-1-challenge/",
-    github: "https://github.com/Adam42288/week-1-challenge",
+    github: "https://github.com/Adam42288/week-1-challenge/",
     technologiesUsed: ["HTML", "CSS", "JavaScript"],
   },
 ];
