00145b50648a163c3d09722137c917d14b9a7c36
Adam <asantillana@apple.com>
2023-03-22 20:06:37 -0500

    added app.js code

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Home from "./components/Home";
+import "./App.css";
+import projects from "./projects";
+
+export default function app() {
+  return <Home projects={projects} />;
+}
