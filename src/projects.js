298ba109d8d4cc0f1ff5dee901758da5cd3bb2c3
Adam <asantillana@apple.com>
2023-03-22 20:11:23 -0500

    added initial project to projects.js

---
+++ src/projects.js
@@ -0,0 +1,14 @@
+const projects = [
+  {
+    id: 1,
+    name: "Accessibility Demo",
+    description:
+      "This is a project we did for a class. It's a demo of how to make a website accessible.",
+    image: require("./assets/accessibilityproject.png"),
+    siteAddress: "https://adam42288.github.io/week-1-challenge/",
+    github: "https://github.com/Adam42288/week-1-challenge",
+    technologiesUsed: ["HTML", "CSS", "JavaScript"],
+  },
+];
+
+export default projects;
